c06982a09cab0dd83b3b00556fea5433f1369873
shrey_chechani <shreychechani@gmail.com>
2026-04-06 22:10:32 +0530

    updated app.py port number

--- app.py
+++ app.py
@@ -238,5 +238,5 @@ def compute_route():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5050))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
